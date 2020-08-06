b1d8588f9207aa9e63cdfabd5a9a2cdec5e4adac
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-08-06 09:55:39 +0100

    Added repositories for booking, course, customer

---
+++ src/main/java/com/codeclan/example/course_booking_system/repositories/BookingRepository.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.course_booking_system.repositories;
+
+import com.codeclan.example.course_booking_system.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+}
---
+++ src/main/java/com/codeclan/example/course_booking_system/repositories/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.course_booking_system.repositories;
+
+import com.codeclan.example.course_booking_system.models.Customer;
+import com.codeclan.example.course_booking_system.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
